0bf3f82bdf36ade597e7b52723f4c3d4df3dc4cb
JReniery <JReniery@DESKTOP-1PCHB5J>
2022-06-21 14:53:32 -0600

    Add project files.

---
+++ Ejercicio1_1/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_1
+{
+    public partial class MainPage : ContentPage
+    {        
+        public MainPage()
+        {
+            InitializeComponent();            
+    }
+
+        private void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            Datos(0);
+        }
+
+        private void btnResta_Clicked(object sender, EventArgs e)
+        {
+            Datos(1);
+        }
+
+        private void btnDiv_Clicked(object sender, EventArgs e)
+        {
+            Datos(2);
+        }
+
+        private void btnMult_Clicked(object sender, EventArgs e)
+        {
+            Datos(3);
+        }       
+        
+        private void Datos(int op)
+        {
+            float num1 = (float)Convert.ToDouble(txbNum1.Text);
+            float num2 = (float)Convert.ToDouble(txbNum2.Text);
+
+            if (op == 2 && num2 == 0)
+            {
+                DisplayAlert("Aviso", "No se puede dividir entre cero", "OK");
+            }
+            else
+            {
+                Navigation.PushAsync(new ResultPage(op, num1, num2));
+            }            
+        }
+    }
+}
---
+++ Ejercicio1_1/ResultPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio1_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResultPage : ContentPage
+    {
+        public ResultPage()
+        {
+            InitializeComponent();
+        }
+        public ResultPage(int op, float num1, float num2)
+        {
+            InitializeComponent();
+            Calcular(op, num1, num2);
+        }
+
+        public void Calcular(int op, float valor1, float valor2)
+        {
+            var resultado = 0f;
+            if (op == 0)
+            {
+                resultado = valor1 + valor2;
+            }
+            else if (op == 1)
+            {
+                resultado = valor1 - valor2;
+            }
+            else if (op == 2)
+            {                
+                resultado = valor1 / valor2;                
+            }
+            else if (op == 3)
+            {
+                resultado = valor1 * valor2;
+            }
+            lblResult.Text = resultado.ToString();
+        }
+    }
+}
